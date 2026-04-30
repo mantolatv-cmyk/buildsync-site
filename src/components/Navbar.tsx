a68a6acfd7111c3ae2429b7fc2405e2104e1c93f
mantolatv-cmyk <mantolatv@github.com>
2026-04-30 17:32:45 -0300

    style: expand navbar to full width and increase spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,23 +6,21 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return (
     <nav className={`${styles.navbar} glass`}>
-      <div className="container">
-        <div className={styles.navContainer}>
-          <div className={styles.logo}>
-            Build<span>Sync</span>
-          </div>
-          
-          <div className={styles.menu}>
-            <a href="#funcionalidades" className={styles.navLink}>Funcionalidades</a>
-            <a href="#beneficios" className={styles.navLink}>Benefícios</a>
-            <a href="#depoimentos" className={styles.navLink}>Depoimentos</a>
-          </div>
+      <div className={styles.navContainer}>
+        <div className={styles.logo}>
+          Build<span>Sync</span>
+        </div>
+        
+        <div className={styles.menu}>
+          <a href="#funcionalidades" className={styles.navLink}>Funcionalidades</a>
+          <a href="#beneficios" className={styles.navLink}>Benefícios</a>
+          <a href="#depoimentos" className={styles.navLink}>Depoimentos</a>
+        </div>
 
-          <div className={styles.actions}>
-            <button className={styles.ctaButton}>
-              Começar Agora
-            </button>
-          </div>
+        <div className={styles.actions}>
+          <button className={styles.ctaButton}>
+            Começar Agora
+          </button>
         </div>
       </div>
     </nav>
